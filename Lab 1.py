2945858142b9ddbd54ae799785f4d96b7f398f3f
Edwin Ascencio <99775680+AscensionCompMeth@users.noreply.github.com>
2022-02-23 09:29:59 -0500

    Add files via upload

---
+++ Lab 1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 23 09:01:52 2022
+
+@author: Edwin
+"""
+
+import sys 
+g=9.8
+h= float(input('What is the height'));
+d=(0.5)*g*h**2;
+t=((2*d)/g)**0.5;
+print(d,t)
